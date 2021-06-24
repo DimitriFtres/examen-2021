0d393d657a57fdb741030a54173c337b88b5e8e9
DimitriFtres <dimitrifastres@gmail.com>
2021-06-24 21:42:19 +0200

    correction ex1

--- function/function_liste.php
+++ function/function_liste.php
@@ -14,22 +14,14 @@ function prendreLestransactionsDuMois($transactions, $contacts){
         $tabDate = explode('/', $value["date"]);
         if($tabDate[1] == $dateDuJour["mon"]){
             $montant = $value["montant"];
-            if($value["montant"] < 0){
                 foreach($contacts as $k => $v){
                 //nom
-                    if($value["destinataire"] == $v["id"]){
+                    if($value["destinataire"] === $v["id"]){
                         $nom = $v["nom"]. ' ' . $v["prenom"];
                         $nom = strtoupper($nom);
                     }  
                 }
-            }else{
-                foreach($contacts as $k => $v){
-                    if($value["emetteur"] == $v["id"]){
-                        $nom = $v["nom"]. ' ' . $v["prenom"];
-                        $nom = strtoupper($nom);     
-                    }
-                }
-            }
+            
             foreach($contacts as $k => $v){
                 //compte
                 $compte = $v["compte"];
@@ -37,12 +29,12 @@ function prendreLestransactionsDuMois($transactions, $contacts){
                 $compte = substr_replace($compte, ' ', 4, 0);
                 $compte = substr_replace($compte, ' ', 9, 0);
                 $compte = substr_replace($compte, ' ', 14, 0);
-                $informationParTransaction[$i] = array();
-                array_push($informationParTransaction[$i], $value["date"], $nom, $montant, $compte);
-                $i++;
-
             }
+            $informationParTransaction[$i] = array();
+            array_push($informationParTransaction[$i], $value["date"], $nom, $montant, $compte);
+            $i++;
         }
+        
 
     }
     return $informationParTransaction;
